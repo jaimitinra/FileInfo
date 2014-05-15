57e54580cfc1d437a345c15445bb0c7d4d653339
jra <jaimitinra@gmail.com>
2014-05-15 10:28:52 +0200

    red RetrieveOneKey

--- FileInfo/FileInfoTests/FileInfoTests.cs
+++ FileInfo/FileInfoTests/FileInfoTests.cs
@@ -23,5 +23,22 @@ namespace FileInfoTests
 
             result.Keys.Count.Should().Be(0);
         }
+
+        [Test]
+        public void RetrieveOneKey()
+        {
+            var service = new FileInfoFinder();
+            var repository = new Mock<FileInfoRepository>();
+            service.Repository = repository.Object;
+
+            repository.Setup(x => x.GetContent(It.IsAny<string>()))
+                .Returns("Quijote");
+
+            var result = service.Analize("pattern");
+
+            result.Keys.Count.Should().Be(1);
+            result.Keys.First().Key.Should().Be("quijote");
+            result.Keys.First().Value.Should().Be(1);
+        }
     }
 }
